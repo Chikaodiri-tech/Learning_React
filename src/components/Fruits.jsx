a7946655a26df275ab6d6c88d3ece70e13eacecc
Chikaodiri Agu <chikaodiri.a@outlook.com>
2025-09-27 16:14:12 +0100

    Create a new file Fruit.jsx and modify the fruits.jsx file

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,12 +1,23 @@
+import Fruit from "./Fruit";
+
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Bananas", "Orange", "Pineapple"]
+  //const fruits = ["Apple", "Mango", "Bananas", "Orange", "Pineapple"]
+  const fruits = [
+    {name: "Apple", price: 10, emoji: "🍎"},
+    {name: "Mango", price: 15, emoji: "🥭"},
+    {name: "Banana", price: 5, emoji: "🍌"},
+    {name: "Orange", price: 8, emoji: "🍊"},
+    {name: "Pineapple", price: 12, emoji: "🍍"},
+    {name: "Grapes", price: 7, emoji: "🍇"}
+  ];
   return (
     <div>
-      <ul>
-        {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
-        ))}
-      </ul>
+        <ul>
+            {fruits.map((fruit) => (
+                <Fruit key={fruit.name} name={fruit.name} price={fruit.price} emoji={fruit.emoji} />
+               // <li key={fruit.name}> {fruit.emoji} {fruit.name} ${fruit.price} </li>
+            ))}
+        </ul>
     </div>
   );
 }
