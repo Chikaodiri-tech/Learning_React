d8fce4e1de68a0c299fdfa983ac35925ef096cba
Chikaodiri Agu <chikaodiri.a@outlook.com>
2025-10-04 18:08:16 +0100

    Added new .jsx files and updated the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Fruits from "./components/Fruits";
+import ConditionalComponent from "./components/ConditionalComponent";
+import Code from "./components/Code";
+import Welcome from "./components/Welcome";
+
 function App() {
   return (
     <div className="App">
-      <Fruits/>
+      <Fruits />
+      <ConditionalComponent />
     </div>
   );
 }
